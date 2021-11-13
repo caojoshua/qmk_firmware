32281403461a419f4c9de817260724bfb8ad9bca
caojoshua <cao.joshua@yahoo.com>
2021-11-12 20:20:06 -0800

    Better mod-tap settings.

--- keyboards/keebio/iris/keymaps/caojoshua/config.h
+++ keyboards/keebio/iris/keymaps/caojoshua/config.h
@@ -19,3 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define USE_I2C
 #define EE_HANDS
+
+#define TAPPING_TERM 150
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
