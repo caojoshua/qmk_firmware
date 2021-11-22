53d539371cc7e5690f83e81eb3e4176c21db8449
caojoshua <cao.joshua@yahoo.com>
2021-11-21 22:03:04 -0800

    Experimental 34 key layout.

--- keyboards/keebio/iris/keymaps/caojoshua/config.h
+++ keyboards/keebio/iris/keymaps/caojoshua/config.h
@@ -20,13 +20,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 
-#define TAPPING_TERM 200
+#define COMBO_COUNT 1
+#define COMBO_TERM 25
+
+#define TAPPING_TERM 150
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 
 #define MOUSEKEY_INTERVAL 16
 #define MOUSEKEY_DELAY 0
-#define MOUSEKEY_TIME_TO_MAX 60
-#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_TIME_TO_MAX 30
+#define MOUSEKEY_MAX_SPEED 10
 #define MOUSEKEY_WHEEL_DELAY 0
